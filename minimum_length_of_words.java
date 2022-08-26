671c9bc352a2daf39e2e00d85b0f423b7a70eeb9
sai teja Kodi <102404293+KSaiteja05@users.noreply.github.com>
2022-08-26 11:47:58 +0530

    import java.util.*; class Main {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         String st=s.next();         System.out.print(st.length());     } }

---
+++ minimum_length_of_words.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        String st=s.next();
+        System.out.print(st.length());
+    }
+}
