073a1d790958fc7fc59939034c6c339c1a8829a5
sai teja Kodi <102404293+KSaiteja05@users.noreply.github.com>
2022-09-01 10:10:22 +0530

    import java.util.*; class Main {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int x=0;         int a[]=new int[n];         for(int i=0;i<n;i++)         {             a[i]=s.nextInt();         }         for(int i=0;i<n;i++)         {             if(a[i]%2==0)             {                 x=a[i];             }         }         System.out.println(x);     } }

---
+++ Last_even_number_in_an_array.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int x=0;
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=s.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]%2==0)
+            {
+                x=a[i];
+            }
+        }
+        System.out.println(x);
+    }
+}
