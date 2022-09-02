a1bdaa4ccb0f306e0f5ef4a95f2ccdad4f60db8f
sai teja Kodi <102404293+KSaiteja05@users.noreply.github.com>
2022-09-02 21:22:21 +0530

    import java.util.*; class Main {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int a[]=new int[n];         for(int i=0;i<n;i++)         {             a[i]=s.nextInt();         }         for(int i=0;i<n;i++)         {             if(a[i]%2==1)             {                 System.out.print(a[i]+" ");             }         }         for(int i=0;i<n;i++)         {             if(a[i]%2==0)             {                 System.out.print(a[i]+" ");             }         }     } }

---
+++ array_of_add_and_even_elements.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=s.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]%2==1)
+            {
+                System.out.print(a[i]+" ");
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]%2==0)
+            {
+                System.out.print(a[i]+" ");
+            }
+        }
+    }
+}
