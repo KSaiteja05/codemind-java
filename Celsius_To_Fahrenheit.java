1132d986421c612a25b7afbfd244ee41cdc37585
sai teja Kodi <102404293+KSaiteja05@users.noreply.github.com>
2022-09-03 17:59:27 +0530

    import java.util.*; class Main {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int l=s.nextInt();         double x=l*1.8+32;         System.out.printf("%.2f",x);     } }

---
+++ Celsius_To_Fahrenheit.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int l=s.nextInt();
+        double x=l*1.8+32;
+        System.out.printf("%.2f",x);
+    }
+}
