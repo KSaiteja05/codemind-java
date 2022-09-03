b173ea3befbb10ff9dd4df85c3c026bace860222
sai teja Kodi <102404293+KSaiteja05@users.noreply.github.com>
2022-09-03 17:38:18 +0530

    import java.util.*; class Main {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         boolean p=false;         int a[]=new int[n];         for(int i=0;i<n;i++)         {             a[i]=s.nextInt();         }         int min=a[0];         for(int i=0;i<n;i++)         {             if(a[i]<min)             {                 min=a[i];             }         }          System.out.println(min);
        }
    }

---
+++ minimum_element_of_an_array.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        boolean p=false;
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=s.nextInt();
+        }
+        int min=a[0];
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]<min)
+            {
+                min=a[i];
+            }
+        }
+         System.out.println(min);
+        
+    }
+}
