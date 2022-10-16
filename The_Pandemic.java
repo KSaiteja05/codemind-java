e8d60c2461f141b55ee2c7351dfb04e7c0f3ec2c
sai teja Kodi <102404293+KSaiteja05@users.noreply.github.com>
2022-10-16 09:47:01 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         if(n==1)         {             System.out.println(0);         }         else if(n==2)         {             System.out.println(1);         }         else         {             System.out.println(2);         }     } }

---
+++ The_Pandemic.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        if(n==1)
+        {
+            System.out.println(0);
+        }
+        else if(n==2)
+        {
+            System.out.println(1);
+        }
+        else
+        {
+            System.out.println(2);
+        }
+    }
+}
