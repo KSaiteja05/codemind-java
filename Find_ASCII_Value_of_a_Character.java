45041500b2d0dbc04363c87f7833f50b0d256324
sai teja Kodi <102404293+KSaiteja05@users.noreply.github.com>
2023-05-01 09:26:37 +0530

    import java.util.*; public class Main {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         char c=s.next().charAt(0);         int x=c;         System.out.println(x);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        char c=s.next().charAt(0);
+        int x=c;
+        System.out.println(x);
+    }
+}
